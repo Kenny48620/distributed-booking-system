df71dc356de8fd7d93b79d4fea421042ea244e63
Kenny48620 <lee.kennyus@gmail.com>
2026-03-20 01:52:04 -0700

    add Redis-based idempotency

--- services/notification-worker/app/consumer.py
+++ services/notification-worker/app/consumer.py
@@ -1,5 +1,6 @@
 import json
 from confluent_kafka import Consumer, KafkaError
+from redis_client import is_event_processed, mark_event_processed
 
 # cereate a Kafka comsumer
 # it connects to the Kafka broker inside the docker-compose network
@@ -20,6 +21,12 @@ consumer = Consumer({
 consumer.subscribe(["booking_created"])
 
 print("Notification worker started. Waiting for events...", flush=True)
+
+# in-memory set to remember which events have already been processed
+# this is the simplest idempotency mechanism for now
+# for the future use, I'll put the event_ids to Redis and DB
+# processed_event_ids = set()
+
 
 try:
     while True:
@@ -43,6 +50,25 @@ try:
         # decode the message value from bytes to string
         # then parse the JSON payload into a Python dictionary
         event = json.loads(msg.value().decode("utf-8"))
+        event_id = event['event_id']
+        
+        print(f"Processing event_id: {event_id}", flush=True)
+
+        # TEMPORARY SOLUTION FOR 'idempotency check'
+        # idempotency check: if we already processed this event_id, skip it
+        # this can prevent re-notify actions
+        # if event_id in processed_event_ids:
+        #     print(f"Duplicate event detected, skipping: {event_id}")
+        #     continue
+        # # otherwise, we mark the event_id
+        # processed_event_ids.add(event_id)
+
+
+        # redis-based idempotency check (I use event_id to check it if it works as expected)
+        # if we've process this event before, stop sending the notification twice
+        if is_event_processed(event_id):
+            print(f"Duplicate event detected in Redis, skipping: {event_id}")
+            continue
 
         # Debug: print the full event for debugging / visibility
         print(f"Received booking event: {event}", flush=True)
@@ -54,6 +80,10 @@ try:
             f"confirmed for user {event['user_id']}",
             flush=True,
         )
+
+        # only mark as processed after side effect succeeds
+        mark_event_processed(event_id)
+        print(f"Marked event as processed in Redis: {event_id}", flush=True)
 
 # handle manual stop (ctrl+c) gracefully
 except KeyboardInterrupt:
