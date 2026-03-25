c7d224ebf18d2bd7d33899924cb17ed9273c2b5b
Kenny48620 <lee.kennyus@gmail.com>
2026-03-25 00:29:41 -0700

    add a thread to handle payment_consumer listening topic payment_succeeded and payment_failed

--- services/booking-service/app/main.py
+++ services/booking-service/app/main.py
@@ -1,11 +1,35 @@
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from .database import Base, engine
 from . import models
 from .routes import router
+from .payment_consumer import start_payment_result_consumer
 
 Base.metadata.create_all(bind=engine)
 
-app = FastAPI()
+
+# has been deprecated
+# @app.on_event("startup")
+# def startup_event():
+#     start_payment_result_consumer()
+# so use FastAPI recommend way to create a thread
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+
+    # start up
+    print("Booking Service is starting up...", flush=True)
+    # start the background Kafka consumer at application startup
+    start_payment_result_consumer()
+
+    # hand control back to FastAPI so it can start serving requests
+    yield
+    # shutdown
+    print("Booking Service is shutting down...", flush=True)
+    # put shutdown cleanup here later if needed
+    
+
+
+app = FastAPI(lifespan=lifespan)
 app.include_router(router)
 
 @app.get("/")
@@ -15,3 +39,8 @@ def root():
 @app.get("/health")
 def health():
     return {"status": "ok"}
+
+
+# @app.on_event("startup")
+# def startup_event():
+#     start_payment_result_consumer()
